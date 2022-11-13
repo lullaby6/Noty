6ca618df29fe2f0f23192e019be4eca38229678f
LucianoBrumer <lucianobrumer5@gmail.com>
2022-11-13 18:03:28 -0300

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://lucianobrumer.github.io/Noty
-# git push -f git@github.com:lucianobrumer/Noty.git main:gh-pages
+git push -f git@github.com:lucianobrumer/Noty.git main:gh-pages
 
 cd -
