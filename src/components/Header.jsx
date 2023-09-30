c099f00ec51e325da47fad7e255c53337a5445c9
LucianoBrumer <lucianobrumer5@gmail.com>
2023-09-29 23:03:46 -0300

    search debounce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,15 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function ({setNotes, setSearch}){
     const showClearInput = useState(false)
+    const debounce = '300'
+    let debounceTimeout = null
+
+    function handleSearch(e){
+        clearTimeout(debounceTimeout)
+        debounceTimeout = setTimeout(() => {
+            setSearch(e.target.value)
+        }, debounce)
+    }
 
     return (
         <header className="flex items-center shadow-md border dark:border-neutral-800 border-b-neutral-300 gap-2 p-2 sticky top-0 left-0 w-full bg-white dark:bg-neutral-900">
@@ -13,7 +22,7 @@ export default function ({setNotes, setSearch}){
                     ? <Icon icon="material-symbols:search" className="text-3xl text-neutral-500 dark:text-neutral-600 h-full"/>
                     : <Icon icon="iconamoon:close" className="pointer-events-none cursor-pointer text-3xl text-neutral-500 dark:text-neutral-600 h-full"/>}
                 </div>
-                <input onChange={e => setSearch(e.target.value)} type="text" placeholder="Search note..." spellCheck="false" autoComplete="off" className="bg-transparent dark:placeholder:text-neutral-800 focus:outline-none placeholder:italic dark:text-white text-neutral-800 text-xl w-full pl-2 p-1"/>
+                <input onChange={handleSearch} type="text" placeholder="Search note..." spellCheck="false" autoComplete="off" className="bg-transparent dark:placeholder:text-neutral-800 focus:outline-none placeholder:italic dark:text-white text-neutral-800 text-xl w-full pl-2 p-1"/>
             </div>
             <ThemeSwitcher></ThemeSwitcher>
         </header>
